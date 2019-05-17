8c036e3cab589bb35b67b3d65be67868aa6daf38
Cecilia Barriga <ceci.abt@gmail.com>
2019-05-17 11:49:34 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,59 @@
+#Interactive Frontend Development Milestone Project
+
+This is a Single-Page Application (SPA), that calls on the Google Maps API and the Google Places API to allow users to search for information about any city they are planning to travel to. 
+
+##UX
+This website is intended for people who are are searching for all the information needed of the city they are planning to visit next. They are able to:
+Select a city
+Find tourist attractions
+Find accommodation
+Find bars, restaurants and coffee shops
+
+I wanted to show images of different cities in the index page with a simple CTA, and then open a modal with the google maps search.
+
+##Features
+I wanted to show images of different cities in the index page with a simple CTA button that when hovered the font becomes lightgray and has a pointer.
+Once the button is clicked then open the Google Maps modal;
+
+In this modal, On the top-left side the user can type the city they are looking for in an input;
+On the top-right side there is a drop-down with the different locations: hotels, bars, restaurant, coffee shops, museums and parks. Once an option is selected, icons will drop on the map according the selection.
+If this icon is clicked, there will be a info window displayed with the name of the establishment and its information.
+
+All the links from the Info Window will open in a new tab as the attribute target="_blank" was used.
+
+
+##Technologies Used
+HTML5 
+CSS3 
+JavaScript 
+Maps JavaScript API 
+Cloud9 IDE
+GitHub 
+
+
+##Testing
+This site was tested in google dev tools including responsiveness on mobile devices and tablets.
+
+According to https://varvy.com/mobile/ 
+
+Site was tested on the following browsers:
+Google Chrome
+Mozilla Firefox
+
+
+##Deployment
+I created a repository on GitHub and through commits using Cloud9 terminal I added the content.
+
+I used HTML and CSS validator, and also JSHint for JavaScript to make sure there were no issues.
+
+
+##Credits
+Content
+Most of the JavaScript has been taken from the Google Maps JavaScript API Documentation.
+The icons were taken from https://mapicons.mapsmarker.com/.
+
+
+##Acknowledgements
+Google Maps Platform Documentation
+Code Institute tutors
+Code Institute Mentor
---
+++ assets/js/maps.js
@@ -0,0 +1,210 @@
+var map, places, infoWindow;
+var markers = [];
+var autocomplete;
+var image;
+var MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/images/marker_green';
+var hostnameRegexp = new RegExp('^https?://.+?/');
+
+
+function initMap() {
+   map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 6,
+    center: { lat: 52.3680, lng: 4.9036 },
+    mapTypeControl: false,
+    panControl: false,
+    zoomControl: true,
+    streetViewControl: false,
+    fullscreenControl: false
+  });
+
+  infoWindow = new google.maps.InfoWindow({
+    content: document.getElementById('info-content')
+  });
+
+  // Cities
+ autocomplete = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */
+    (
+      document.getElementById('autocomplete')), {
+      types: ['(cities)'],
+    });
+  places = new google.maps.places.PlacesService(map);
+
+  autocomplete.addListener('place_changed', onPlaceChanged);
+
+}
+
+// Zoom in for City selected
+function onPlaceChanged() {
+  var place = autocomplete.getPlace();
+  if (place.geometry) {
+    map.panTo(place.geometry.location);
+    map.setZoom(15);
+    search();
+  }
+}
+
+// Search for locations in the city selected
+function search() {
+  var locations = document.getElementById("locations").value;
+  console.log(map.getBounds())
+  var search = {
+    bounds: map.getBounds(),
+    types: [locations]
+  };
+
+  places.nearbySearch(search, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      clearResults();
+      clearMarkers();
+  
+      for (var i = 0; i < results.length; i++) {
+
+        if (locations == "lodging") {
+          image = "assets/images/hotel.png";
+        }
+        else if (locations == "cafe") {
+          image = "assets/images/coffee.png";
+        }
+        else if (locations == "bar") {
+          image = "assets/images/bar.png";
+        }
+        else if (locations == "restaurant") {
+          image = "assets/images/restaurant.png";
+        }
+        else if (locations == "museum") {
+          image = "assets/images/temple-2.png";
+        }
+        else if (locations == "park") {
+          image = "assets/images/tree.png";
+        }
+        // Use marker animation to drop the icons incrementally on the map.
+        markers[i] = new google.maps.Marker({
+          position: results[i].geometry.location,
+          animation: google.maps.Animation.DROP,
+          icon: image
+        });
+        // Info window
+        markers[i].placeResult = results[i];
+        google.maps.event.addListener(markers[i], 'click', showInfoWindow);
+        setTimeout(dropMarker(i), i * 10);
+        addResult(results[i], i);
+      }
+    }
+  });
+}
+
+function clearMarkers() {
+  for (var i = 0; i < markers.length; i++) {
+    if (markers[i]) {
+      markers[i].setMap(null);
+    }
+  }
+  markers = [];
+}
+
+
+function dropMarker(i) {
+  return function() {
+    markers[i].setMap(map);
+  };
+}
+
+function addResult(result, i) {
+  var results = document.getElementById('results');
+  var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
+  var markerIcon = MARKER_PATH + markerLetter + '.png';
+
+  var tr = document.createElement('tr');
+  tr.style.backgroundColor = (i % 2 === 0 ? '#F0F0F0' : '#FFFFFF');
+  tr.onclick = function() {
+    google.maps.event.trigger(markers[i], 'click');
+  };
+
+  var iconTd = document.createElement('td');
+  var nameTd = document.createElement('td');
+  var icon = document.createElement('img');
+  icon.src = markerIcon;
+  icon.setAttribute('class', 'placeIcon');
+  icon.setAttribute('className', 'placeIcon');
+  var name = document.createTextNode(result.name);
+  iconTd.appendChild(icon);
+  nameTd.appendChild(name);
+  tr.appendChild(iconTd);
+  tr.appendChild(nameTd);
+  results.appendChild(tr);
+}
+
+function clearResults() {
+  var results = document.getElementById('results');
+  while (results.childNodes[0]) {
+    results.removeChild(results.childNodes[0]);
+  }
+}
+
+// Get the place details for a selected hotel in an info window.
+function showInfoWindow() {
+  var marker = this;
+  places.getDetails({ placeId: marker.placeResult.place_id },
+    function(place, status) {
+      if (status !== google.maps.places.PlacesServiceStatus.OK) {
+        return;
+      }
+      infoWindow.open(map, marker);
+      buildIWContent(place);
+    });
+}
+
+// Load the place information into the HTML elements used by the info window.
+function buildIWContent(place) {
+  document.getElementById('iw-icon').innerHTML = '<img class="hotelIcon" ' +
+    'src="' + place.icon + '"/>';
+  document.getElementById('iw-url').innerHTML = '<b><a href="' + place.url +
+    '" target="_blank" >' + place.name + '</a></b>';
+  document.getElementById('iw-address').textContent = place.vicinity;
+
+  if (place.formatted_phone_number) {
+    document.getElementById('iw-phone-row').style.display = '';
+    document.getElementById('iw-phone').textContent =
+      place.formatted_phone_number;
+  }
+  else {
+    document.getElementById('iw-phone-row').style.display = 'none';
+  }
+
+  // Assign a five-star rating , using a black star ('&#10029;')
+  // to indicate the rating the location has earned, and a white star ('&#10025;')
+  // for the rating points not achieved.
+  if (place.rating) {
+    var ratingHtml = '';
+    for (var i = 0; i < 5; i++) {
+      if (place.rating < (i + 0.5)) {
+        ratingHtml += '&#10025;';
+      }
+      else {
+        ratingHtml += '&#10029;';
+      }
+      document.getElementById('iw-rating-row').style.display = '';
+      document.getElementById('iw-rating').innerHTML = ratingHtml;
+    }
+  }
+  else {
+    document.getElementById('iw-rating-row').style.display = 'none';
+  }
+
+  // The regexp isolates the first part of the URL (domain plus subdomain)
+  // to give a short URL for displaying in the info window.
+  if (place.website) {
+    var fullUrl = place.website;
+    var website = hostnameRegexp.exec(place.website);
+    if (website === null) {
+      website = 'http://' + place.website + '/';
+      fullUrl = website;
+    }
+    document.getElementById('iw-website-row').style.display = '';
+    document.getElementById('iw-website').textContent = website;
+  }
+  else {
+    document.getElementById('iw-website-row').style.display = 'none';
+  }
+}
